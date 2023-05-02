d3393f571d442a2603643eb789178879ac0b850e
Francesco Bozzotta (aka xFr4xx) <francesco.bozzotta@gmail.com>
2023-05-02 22:54:53 +0200

    fix ci integrations

--- libs/sio/auth/src/index.ts
+++ libs/sio/auth/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/sio-auth.module';
 export * from './lib/interfaces';
 export * from './lib/guards';
 export * from './lib/services/plugins'
+export * from './lib/components/badge';
+export * from './lib/components/login';
