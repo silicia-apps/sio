5cae01846e9b89a8f1fd70f78e87ae0a321313e5
Francesco Bozzotta <39646929+silicia-apps@users.noreply.github.com>
2025-05-02 18:32:28 +0000

    add chat plugin

---
+++ libs/chat/src/lib/chat/chat.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-chat',
+  imports: [CommonModule],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.css',
+})
+export class ChatComponent {}
